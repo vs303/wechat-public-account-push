a2be3286496636f8f1f76591105074d051612e4a
vs303 <77723806+vs303@users.noreply.github.com>
2022-08-29 14:53:44 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -135,13 +135,13 @@ export const getBirthdayMessage = () => {
     birthdayList.forEach(birthday => {
         let birthdayMessage = null
         // 获取距离下次生日的时间
-        const nextBir = dayjs(year+'-'+ birthday.date).diff(dayjs(NOW_MONTHS_AGO ), 'day')
+        const nextBir = dayjs(year+'-'+ birthday.date).diff(dayjs(),'day')
         if (nextBir === 0) {
             birthdayMessage = `今天是 ${birthday.name} 生日哦，祝${birthday.name}生日快乐！`
         } else if (nextBir > 0 ) {
             birthdayMessage = `距离 ${birthday.name} 的生日还有${nextBir}天`
         }  
-        resMessage += `${year+'-'+ birthday.date} \n`
+        resMessage += `${nextBir} \n`
         // 存储数据
         if (birthdayMessage===0) {
             resMessage += `${birthdayMessage} \n`
