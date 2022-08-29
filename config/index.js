37b2fc5d7478c7f913b872e6a76e70e9891a565c
vs303 <77723806+vs303@users.noreply.github.com>
2022-08-29 10:50:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     // 模板消息id
     TEMPLATE_ID: "Kpf4oe33in2zzmnY-Vfv1ZzxzCkBahPbkWy5iGBc35A",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["18346041654"],
      
     /**
      * 信息配置
