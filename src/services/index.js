dc826c584341c6012c80633ebd9adeee22715d65
vs303 <77723806+vs303@users.noreply.github.com>
2022-08-29 15:03:54 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -3,7 +3,7 @@ import { CITY_INFO, TYPE_LIST } from '../store/index.js'
 import axios from 'axios'
 import dayjs from 'dayjs'
 import { randomNum } from '../utils/index.js'
-import calendarFormatter from '../../config/lunarDay.js'
+import calendarFormatter from '/lunarDay.js'
 /**
  * 获取 accessToken
  * @returns accessToken
